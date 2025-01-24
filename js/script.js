aa0105e793698866099b924b58afa3be96a9fec8
SINU-GUPTA <sk5784708@gmail.com>
2025-01-24 12:45:20 +0530

    structure going on

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// here i write js function for read more functionality 
+
+document.getElementById("readMoreBtn").addEventListener("click", function () {
+    const extraContent = document.querySelector(".extra-content");
+    const isHidden = extraContent.classList.contains("d-none");
+  
+    if (isHidden) {
+      extraContent.classList.remove("d-none"); // Show the hidden content
+      this.textContent = "Show Less"; // Change button text
+    } else {
+      extraContent.classList.add("d-none"); // Hide the content again
+      this.textContent = "Read More"; // Reset button text
+    }
+  });
+  
+  
